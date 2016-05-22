acce2adeeacebcd5b8e162661c7252f34587783d
Evan <evanjfrancis@gmail.com>
2016-05-22 15:06:27 -0700

    setup method openDoc so authority doesnt get created until client wants to open it. created two demo documents to showcase multiple authorities working. removed blaze templates to just attach to raw dom instances

--- demo/client/main.js
+++ demo/client/main.js
@@ -1,38 +1,62 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { ProseMeteorEditor } from 'meteor/prosemeteor:prosemirror'
+import { ProseMeteorEditor } from 'meteor/prosemeteor:prosemirror';
+import { Meteor } from 'meteor/meteor';
 
-import './main.html';
-
-Template.prosemeteor.onCreated(function helloOnCreated() {
-  this.msg = new ReactiveVar("");
-  if(ProseMeteorEditor) {
-    this.msg.set("Prosemirror has loaded successfully");
-  } else {
-    this.msg.set("Prosemirror has NOT loaded successfully");
-  };
-});
-
-Template.prosemeteor.helpers({
-  msg() {
-    return Template.instance().msg.get();
-  },
-});
-
-
-Template.prosemeteor.onRendered(function helloOnCreated() {
-  let editor = new ProseMeteorEditor({
-    docId: 'proofOfConceptDocId',
+Meteor.startup(function() {
+  let editor1 = new ProseMeteorEditor({
+    docId: 'proofOfConceptDocId1',
     proseMirrorOptions: {
-      place: document.querySelector(".full"),
+      place: document.getElementById('prosemeteor-poc-1'),
       menuBar: true,
       autoInput: true,
       tooltipMenu: {selectedBlockMenu: true}
     }
   });
-  // above should use plugin from Prosemeteor package with option enabled
-  // prosemeteor plugin takes the desired document id as a parameter
-  // that plugin that wraps the Prosemeteor instance with the collab module as the website demo does:
-  // https://github.com/ProseMirror/website/blob/master/src/demo/collab/client/collab.js#L50
 
+  let editor2 = new ProseMeteorEditor({
+    docId: 'proofOfConceptDocId2',
+    proseMirrorOptions: {
+      place: document.getElementById('prosemeteor-poc-2'),
+      menuBar: true,
+      autoInput: true,
+      tooltipMenu: {selectedBlockMenu: true}
+    }
+  });
 });
+
+
+//
+//
+// import './main.html';
+//
+// Template.prosemeteor.onCreated(function onCreated() {
+//   this.msg = new ReactiveVar("");
+//   if(ProseMeteorEditor) {
+//     this.msg.set("Prosemirror has loaded successfully");
+//   } else {
+//     this.msg.set("Prosemirror has NOT loaded successfully");
+//   };
+// });
+//
+// Template.prosemeteor.helpers({
+//   msg() {
+//     return Template.instance().msg.get();
+//   },
+// });
+//
+//
+// Template.prosemeteor.onRendered(function onRendered() {
+//   let editor = new ProseMeteorEditor({
+//     docId: 'proofOfConceptDocId1',
+//     proseMirrorOptions: {
+//       place: document.querySelector(".full"),
+//       menuBar: true,
+//       autoInput: true,
+//       tooltipMenu: {selectedBlockMenu: true}
+//     }
+//   });
+//   // above should use plugin from Prosemeteor package with option enabled
+//   // prosemeteor plugin takes the desired document id as a parameter
+//   // that plugin that wraps the Prosemeteor instance with the collab module as the website demo does:
+//   // https://github.com/ProseMirror/website/blob/master/src/demo/collab/client/collab.js#L50
+//
+// });
