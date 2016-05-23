7f44e6b813bcfcb5323d38e3cfd60d3b591a6d0b
George Kellerman <funkyeah@gmail.com>
2016-05-23 11:38:19 -0500

    Revert "meteor client side operation with doc from server fixture"
    This reverts commit fe98a0f586f4535a543f988f7eb98ed049f8128f.

--- demo/client/main.js
+++ demo/client/main.js
@@ -5,48 +5,30 @@ import { Prosepipe } from 'meteor/prosemeteor:prosemirror'
 
 
 import './main.html';
-
-const Documents = new Mongo.Collection('Documents');
+export const Documents = new Mongo.Collection('Documents');
 
 Template.prosemeteor.onCreated(function helloOnCreated() {
-  this.subscribe("documents.documents");
-
+  
+  this.msg = new ReactiveVar("");
   if(Prosemirror) {
-    console.log("Prosemirror has loaded successfully");
+    this.msg.set("Prosemirror has loaded successfully");
   } else {
-    console.log("Prosemirror has NOT loaded successfully");
+    this.msg.set("Prosemirror has NOT loaded successfully");
   };
 
 });
 
 
 
-Template.prosemeteor.onRendered(function helloOnRendered() {
-  this.autorun(() => {
-    if (this.subscriptionsReady()) {
-      collabDoc = Documents.findOne();
-      collabDocId = collabDoc._id
-      pmDoc = collabDoc.doc
-
-      let editor = new Prosemirror({
-        place: document.querySelector(".full"),
-        menuBar: true,
-        autoInput: true,
-        tooltipMenu: {selectedBlockMenu: true},
-        doc: pmDoc,
-        docFormat: "json",
-        // TODO make plugin where one case pass id of doc to connect to authority like:
-        // prosemeteor: {
-        //   id: collabDocId 
-        // }
-      });
-
-      // for now, instead of prosemirror plugin, creating connection to serverin demo below
-      // https://github.com/ProseMirror/website/blob/master/src/demo/collab/client/collab.js#L23
-
-    }
+Template.prosemeteor.onRendered(function helloOnCreated() {
+  let editor = new Prosemirror({
+    place: document.querySelector(".full"),
+    menuBar: true,
+    autoInput: true,
+    tooltipMenu: {selectedBlockMenu: true},
+    doc: "Placeholder content",
+    docFormat: "html",
   });
-
   // above should use plugin from Prosemeteor package with option enabled
   // prosemeteor plugin takes the desired document id as a parameter
   // that plugin that wraps the Prosemeteor instance with the collab module as the website demo does:
@@ -63,6 +45,7 @@ Template.prosemeteor.onRendered(function helloOnRendered() {
   // editor.setDoc(editor.schema.nodeFromJSON(data.doc))
   // editor.setOption("collab", {version: data.version})
   // this.collab = this.pm.mod.collab
+
 
 
 
