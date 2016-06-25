e96c9d87df601b85f539608d65747a5e72fe1632
Evan <evanjfrancis@gmail.com>
2016-06-25 13:55:58 -0700

    setting up authority-registry to run health checks when registering a new authority server

--- package.js
+++ package.js
@@ -23,6 +23,7 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('rocketchat:streamer@0.3.5');
   api.use('mdg:validated-method@1.1.0');
+  api.use('http');
   api.mainModule('lib/client/client-main.js', 'client');
   api.mainModule('lib/server/server-main.js', 'server');
 });
