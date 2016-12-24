2a8ba05338aec8064cded9bdd10c6a19a917169d
Evan Francis <Evan_Francis@intuit.com>
2016-12-23 19:07:54 -0500

    client demo wait for meteor connection ready

--- demo/client/main.js
+++ demo/client/main.js
@@ -3,28 +3,32 @@ import { Meteor } from 'meteor/meteor';
 
 const docActivityTimeoutCallback = ({ docId }) => {
   alert(`Document ${docId} activity timeout reached, shutting down server session`);
-}
+};
 
 Meteor.startup(function() {
-  let editor1 = new ProseMeteorEditor({
-    docId: 'proofOfConceptDocId1',
-    onDocActivityTimeout: docActivityTimeoutCallback,
-    proseMirrorOptions: {
-      place: document.getElementById('prosemeteor-poc-1'),
-      menuBar: true,
-      autoInput: true,
-      tooltipMenu: {selectedBlockMenu: true}
-    }
-  });
+  if(Meteor.status().status === 'connected') {
+    let editor1 = new ProseMeteorEditor({
+      docId: 'proofOfConceptDocId1',
+      onDocActivityTimeout: docActivityTimeoutCallback,
+      proseMirrorOptions: {
+        place: document.getElementById('prosemeteor-poc-1'),
+        menuBar: true,
+        autoInput: true,
+        tooltipMenu: {selectedBlockMenu: true}
+      }
+    });
 
-  let editor2 = new ProseMeteorEditor({
-    docId: 'proofOfConceptDocId2',
-    onDocActivityTimeout: docActivityTimeoutCallback,
-    proseMirrorOptions: {
-      place: document.getElementById('prosemeteor-poc-2'),
-      menuBar: true,
-      autoInput: true,
-      tooltipMenu: {selectedBlockMenu: true}
-    }
-  });
+    let editor2 = new ProseMeteorEditor({
+      docId: 'proofOfConceptDocId2',
+      onDocActivityTimeout: docActivityTimeoutCallback,
+      proseMirrorOptions: {
+        place: document.getElementById('prosemeteor-poc-2'),
+        menuBar: true,
+        autoInput: true,
+        tooltipMenu: {selectedBlockMenu: true}
+      }
+    });
+  } else {
+    console.log('Meteor connection not ready yet');
+  }
 });
