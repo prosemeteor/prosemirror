253ceccfb5887445ec09b9a132543098d74ec02e
Evan Francis <Evan_Francis@intuit.com>
2016-12-23 21:29:39 -0500

    added auth-hooks class, added git url in package.js

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Prosemirror integration for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/prosemeteor/prosemirror',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
